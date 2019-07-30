2f2e71be34c8fda521ef881cff2766e38b0f2b4c
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-07-31 00:53:20 +0100

    Progress 7

--- PrependTillN.go
+++ PrependTillN.go
@@ -3,17 +3,18 @@ package str
 // PrependTillN () repeatedly prepends a prepend to a subject, until the length of the subject is at
 // least N chars in size.
 //
-// INPUTS
+// Inputs
 //
 // input 0: The subject.
 //
 // input 1: The prepend. If value is an empty string, input 0 is returned.
 //
-// input 2: Size N. The highest value of N is the highest value of type int. However, when the value
-// of N is very high (like 1000000000 (1 billion)), this function could block indefinitely, due to
-// the way Go works (behaviour has been confirmed in Go versions <= 1.12.4). If this function starts
-// to block with high values of N, you can lower your N, but increase the number of times the
-// function is called. So, instead of this:
+// input 2: Mininum length (N) input 0 must attain before the function stops preprending input 1 to
+// input 0. The highest value of N is the highest value of type int. However, when the value of N is
+// very high (like 1000000000 (1 billion)), this function could block indefinitely, due to the way Go
+// works (behaviour has been confirmed in Go versions <= 1.12.4). If this function starts to block
+// with high values of N, you can lower your N, but increase the number of times the function is
+// called. So, instead of this:
 //
 // text = str.PrependTillN (text, "hi!", 1000000000)
 //
@@ -22,7 +23,7 @@ package str
 // text = str.PrependTillN (text, "hi!", 500000000)
 // text = str.PrependTillN (text, "hi!", 500000000)
 //
-// OUTPTS
+// Outpts
 //
 // outpt 0: Result of operation.
 func PrependTillN (subject, prepend string, minLen int) (string) {
